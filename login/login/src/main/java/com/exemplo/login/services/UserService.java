f5a8cbe8c829719957dbd9397962bdeda0df0f61
Caiodev2 <caioadrean@gmail.com>
2025-11-01 15:27:11 -0300

    add método registerUser

--- login/login/src/main/java/com/exemplo/login/services/UserService.java
+++ login/login/src/main/java/com/exemplo/login/services/UserService.java
@@ -1,11 +1,16 @@
 package com.exemplo.login.services;
 
+import com.exemplo.login.entites.User;
 import com.exemplo.login.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
-
     @Autowired
-    private UserRepository userRepository;
+    UserRepository userRepository;
 
+    public User registerUser (User user){
+        return userRepository.save(user);
+    }
 }
