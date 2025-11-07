498308c1e60c36cfe66456726681cde8d44d19af
Caiodev2 <caioadrean@gmail.com>
2025-11-07 15:05:41 -0300

    adiciona Handler exception

---
+++ login/login/src/main/java/com/exemplo/login/controllers/exception/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.exemplo.login.controllers.exception;
+
+import com.exemplo.login.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(),status.value(),e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
