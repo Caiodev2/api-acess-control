9faa5bb7107fe51cabc6823c67256b7ca5e6d058
Caiodev2 <caioadrean@gmail.com>
2026-09-16 14:12:40 -0300

    add User Details Service

---
+++ login/login/src/main/java/com/exemplo/login/config/UserDetailsServiceImp.java
@@ -0,0 +1,23 @@
+package com.exemplo.login.config;
+
+import com.exemplo.login.entites.User;
+import com.exemplo.login.repositories.UserRepository;
+import com.exemplo.login.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+
+        return new UserDetailsImp(user);
+    }
+}
