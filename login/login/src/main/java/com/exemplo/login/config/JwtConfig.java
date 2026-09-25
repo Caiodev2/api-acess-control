455d279b11288963b1af1703a51b4e4ba00868ba
Caiodev2 <caioadrean@gmail.com>
2026-09-25 08:09:31 -0300

    refactor: remove unused imports

--- login/login/src/main/java/com/exemplo/login/config/JwtConfig.java
+++ login/login/src/main/java/com/exemplo/login/config/JwtConfig.java
@@ -4,9 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.exemplo.login.entites.User;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.Optional;
--- login/login/src/main/java/com/exemplo/login/config/JwtUserData.java
+++ login/login/src/main/java/com/exemplo/login/config/JwtUserData.java
@@ -3,5 +3,4 @@ package com.exemplo.login.config;
 
 
 public record JwtUserData(Long userId, String email) {
-
 }
